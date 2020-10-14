96be2296c4064fd6a64d0412d941b408169b6800
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-10-14 13:11:20 -0700

    heroku db

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use(routes);
 
 // turn on connection to db and server
 // force associations aka foreign keys, table will be dropped and recreated if new foreign keys are estabilished
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
