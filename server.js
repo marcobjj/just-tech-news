cba707be8ef9a56285ac88e725d046408b7bdd31
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-10-20 13:59:03 -0700

    test and helpers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -21,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 
 
